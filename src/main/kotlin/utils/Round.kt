2646a9c23c2b1cc415522108e3d1eb6a028d23a1
Biryukov239 <gregory239@mail.ru>
2024-05-18 17:45:45 +0300

    some documentation to unclear functions

--- src/main/kotlin/utils/Round.kt
+++ src/main/kotlin/utils/Round.kt
@@ -1,10 +1,22 @@
 package org.example.utils
 
 object Round {
+    /**
+     * rounds up division first by second
+     * @param first the Int number
+     * @param second the Int number
+     * @return the Int number that first / second rounded up, if it not divides entirely
+     */
     fun roundUp(first: Int, second: Int): Int {
         return (first - 1 + second) / second
     }
 
+    /**
+     * complements a number to a higher multiple after rounding up
+     * @param first the Int number
+     * @param second the int number
+     * @return the Int number that is a higher multiple number
+     */
     fun fit(first: Int, second: Int): Int {
         return roundUp(first, second) * second
     }
