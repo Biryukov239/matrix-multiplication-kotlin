84c0d68ac1aabb1a5d6eea9a96289f7a529f3f37
Biryukov239 <gregory239@mail.ru>
2024-05-18 11:06:15 +0300

    unreadable variables are fixed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,37 +8,37 @@ import com.xenomachina.argparser.ArgParser
 
 
 class MyArgs(parser: ArgParser) {
-    val m by parser.storing("-m", help = "Height of the first matrix") { toInt() }
-    val k by parser.storing("-k", help = "Weight of the first matrix") { toInt() }
-    val n by parser.storing("-n", help = "Weight of the second matrix") { toInt() }
-    val fst by parser.storing("-f", help = "Float matrix after -f") { toString() }
-    val snd by parser.storing("-s", help = "Float matrix after -s") { toString() }
+    val firstRowCount by parser.storing("-m", help = "Height of the first matrix") { toInt() }
+    val firstColumnCount by parser.storing("-k", help = "Weight of the first matrix") { toInt() }
+    val secondColumnCount by parser.storing("-n", help = "Weight of the second matrix") { toInt() }
+    val firstMatrixInput by parser.storing("-f", help = "Float matrix after -f") { toString() }
+    val secondMatrixInput by parser.storing("-s", help = "Float matrix after -s") { toString() }
 }
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
         ArgParser(args).parseInto(::MyArgs).run {
-            println("Value after -m: $m")
-            println("Value after -k: $k")
-            println("Value after -n: $n")
-            println("Values after -f: $fst")
-            println("Values after -s: $snd")
-            val firstMat = fst.split(",").map { it.toFloat() }.toFloatArray()
-            val secondMat = snd.split(",").map { it.toFloat() }.toFloatArray()
-            val gpuMultiplier = GPUMultiplier(firstMat, secondMat, m, k, n)
+            println("Value after -m: $firstRowCount")
+            println("Value after -k: $firstColumnCount")
+            println("Value after -n: $secondColumnCount")
+            println("Values after -f: $firstMatrixInput")
+            println("Values after -s: $secondMatrixInput")
+            val firstMat = firstMatrixInput.split(",").map { it.toFloat() }.toFloatArray()
+            val secondMat = secondMatrixInput.split(",").map { it.toFloat() }.toFloatArray()
+            val gpuMultiplier = GPUMultiplier(firstMat, secondMat, firstRowCount, firstColumnCount, secondColumnCount)
             gpuMultiplier.calculate()
-            val cpuMultiplier: Multiplier = CPUMultiplier(firstMat, secondMat, m, k, n)
+            val cpuMultiplier: Multiplier = CPUMultiplier(firstMat, secondMat, firstRowCount, firstColumnCount, secondColumnCount)
             cpuMultiplier.calculate()
-            val resGPU = gpuMultiplier.resultMat
-            val resCPU = cpuMultiplier.resultMat
-            for (i in 0 until m * n) {
-                if (abs((resGPU[i] - resCPU[i]).toDouble()) > 0.01f) {
+            val multiplicationGPUResult = gpuMultiplier.resultMat
+            val multiplicationCPUResult = cpuMultiplier.resultMat
+            for (i in 0 until firstRowCount * secondColumnCount) {
+                if (abs((multiplicationGPUResult[i] - multiplicationCPUResult[i]).toDouble()) > 0.01f) {
                     println("Error at index $i")
                 }
             }
-            println(resGPU.contentToString())
-            println(resCPU.contentToString())
+            println(multiplicationGPUResult.contentToString())
+            println(multiplicationCPUResult.contentToString())
         }
 
     }
